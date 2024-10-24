01945fa5d605a5b70d77068c64a9337e7a50dcfc
alumnoj <jsolcor457@g.educaand.es>
2024-10-24 21:49:30 +0200

    Martes

---
+++ src/Tema2/RelacionUnidad2/Ejercicio2.java
@@ -0,0 +1,14 @@
+package Tema2.RelacionUnidad2;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+
+        /*
+        Programa que lea un número por teclado e indique si es múltiplo de 2, 3 5 o 7.
+         */
+
+
+
+    }
+}
