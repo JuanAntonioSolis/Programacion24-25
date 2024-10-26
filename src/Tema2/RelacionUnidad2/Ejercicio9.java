420aef5fdae7724e564e3876f117e401cc203187
juana <jsolcor457@g.educaand.es>
2024-10-26 23:49:06 +0200

    Sabado

--- src/Tema2/RelacionUnidad2/Ejercicio9.java
+++ src/Tema2/RelacionUnidad2/Ejercicio9.java
@@ -5,8 +5,11 @@ public class Ejercicio9 {
     public static void main(String[] args) {
 
         /*
-
+        Crea un metodo que, dado un número entero pasado como parámetro, devuelva cuántos digitos lo forman.
+        P.ej: :el número 54326 consta de 5 dígitos. Pista: cuántas veces se puede dividir entre 10.
          */
+
+
 
     }
 }
