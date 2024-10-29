9fcac21e4d6a22632ae65771e63e18ba71f68e00
alumnoj <jsolcor457@g.educaand.es>
2024-10-29 19:50:29 +0100

    Martes

--- src/Tema2/PracticaEquipos/Ejercicio2.java
+++ src/Tema2/PracticaEquipos/Ejercicio2.java
@@ -44,6 +44,28 @@ public class Ejercicio2 {
         return menor;
     }
 
+    public static int sumaTirada(int dado1, int dado2, int dado3, int dado4, int dado5) {
+        int total = 0;
+        int mayor = maximoTirada(dado1, dado2, dado3, dado4, dado5);
+        int menor = minimoTirada(dado1, dado2, dado3, dado4, dado5);
+        if (dado1 != mayor && dado1 != menor) {
+            total += dado1;
+        }
+        if (dado2 != mayor && dado2 != menor) {
+            total += dado2;
+        }
+        if (dado3 != mayor && dado3 != menor) {
+            total += dado3;
+        }
+        if (dado4 != mayor && dado4 != menor) {
+            total += dado4;
+        }
+        if (dado5 != mayor && dado5 != menor) {
+            total += dado5;
+        }
+        return total;
+    }
+
     public static void main(String[] args) {
 
         /*
@@ -74,14 +96,18 @@ public class Ejercicio2 {
         int menorHumerus = minimoTirada(tirada1H,tirada2H,tirada3H,tirada4H,tirada5H);
 
 
+
+
         System.out.printf("%d %d %d %d %d",tirada1C,tirada2C,tirada3C,tirada4C,tirada5C);
         System.out.println();
         System.out.println("Mayor C: " + mayorCubitus);
         System.out.println("Menor C: " + menorCubits);
+        System.out.println("Total C: " + sumaTirada(tirada1C,tirada2C,tirada3C,tirada4C,tirada5C));
         System.out.printf("%d %d %d %d %d",tirada1H,tirada2H,tirada3H,tirada4H,tirada5H);
         System.out.println();
         System.out.println("Mayor H: " + mayorHumerus);
         System.out.println("Menor H: " + menorHumerus);
+        System.out.println("Total H: " + sumaTirada(tirada1H,tirada2H,tirada3H,tirada4H,tirada5H));
 
 
     }
