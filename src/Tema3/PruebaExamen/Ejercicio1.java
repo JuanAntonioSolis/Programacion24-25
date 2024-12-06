b472c4d2ba97712a26941f1da97027ccf6ff0167
juana <jsolcor457@g.educaand.es>
2024-12-06 19:44:41 +0100

    Martes

---
+++ src/Tema3/PruebaExamen/Ejercicio1.java
@@ -0,0 +1,24 @@
+package Tema3.PruebaExamen;
+
+/*
+Nos piden crear una matriz 20x20 de números enteros que inicialmente rellenamos
+de valores aleatorios (1-100), nos piden hacer un menú con estas opciones:
+
+1. Intercambiar fila. Te pedirá dos números de fila e intercambiará los valores
+de la primera por los valores de la segunda.
+2. Suma de una columna que se pedirá al usuario (controlar que elija una correcta)
+3. Sumar la diagonal principal y la diagonal inversa, y mostar las 2 sumas.
+4. Pintar las coordenadas i,j donde se encuentra el mayor elemento de la matriz.
+5. Pintar la matriz traspuesta.
+6. Muestra la matriz con las filas ordenadas de mayor a menor.
+7. Salir
+ */
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
