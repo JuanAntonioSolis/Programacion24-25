368e22800238d3f0c607fb203aa6d743505ee746
juana <jsolcor457@g.educaand.es>
2024-12-10 15:31:44 +0100

    Martes

--- src/Tema3/PruebaExamen/Ejercicio2.java
+++ src/Tema3/PruebaExamen/Ejercicio2.java
@@ -23,6 +23,47 @@ public class Ejercicio2 {
         return suma;
     }
 
+
+    public static int comprobacion(int t1, int t2) {
+
+        int ganador = 0;
+        int resultado = 0;
+
+        if ((t1 == 7 || t1 == 11) & (t2 !=7 || t2 != 11) ) {
+            ganador = t1;
+        } else if((t1 != 7 || t1 != 11) & (t2 ==7 || t2 == 11)) {
+            ganador = t2;
+        } else if (t1 == 2 || t1 == 3 || t1 == 12 & t2 != 2 || t2 != 3 || t2 != 12) {
+            ganador = t2;
+        } else if (t1 != 2 || t1 != 3 || t1 != 12 & t2 == 2 || t2 == 3 || t2 == 12) {
+            ganador = t1;
+        } else if (t1 != 7 || t1 != 11 || t1 != 2 || t1 != 3 || t1 != 12 ){
+            resultado = t1;
+            while (t1 != resultado || t1  == 7){
+                t1 = tiradaDados();
+                if (t1 == resultado)
+                    ganador = t1;
+                else if (t1 == 7)
+                    ganador = t2;
+            }
+        } else if (t2 != 7 || t2 != 11 || t2 != 2 || t2 != 3 || t2 != 12 ){
+            resultado = t2;
+            while (t2 != resultado || t2  == 7){
+                t2 = tiradaDados();
+                if (t2 == resultado)
+                    ganador = t2;
+                else if (t2 == 7)
+                    ganador = t1;
+            }
+        }
+
+        return ganador;
+    }
+
+
+
+    /*
+
     public static boolean comprobacion7_11(int tirada) {
 
         if (tirada == 7 || tirada == 11){
@@ -39,7 +80,7 @@ public class Ejercicio2 {
         return false;
     }
 
-    /*
+
     IBAS POR AQUI
     public static boolean comprobacionGuardar(int tirada) {
         if (tirada == 1 || tirada == 4 || tirada == 5)
@@ -58,6 +99,32 @@ public class Ejercicio2 {
         int victoriasJugador2 = 0;
         int partidas = 10;
 
+        for (int i = 0; i < partidas; i++) {
+            jugador1 = tiradaDados();
+            jugador2 = tiradaDados();
+
+            System.out.println("Tirada jugador 1: " + jugador1);
+            System.out.println("Tirada jugador 2: " + jugador2);
+
+
+            if (comprobacion(jugador1, jugador2) == jugador1) {
+                System.out.println("Gana el jugador 1");
+                victoriasJugador1++;
+            } else if (comprobacion(jugador1, jugador2) == jugador2) {
+                System.out.println("Gana el jugador 2");
+                victoriasJugador2++;
+            } else if (jugador1 == jugador2) {
+                System.out.println("Es un empate");
+            }
+
+
+            System.out.println("");
+        }
+
+        System.out.println("El jugador 1 ha ganado: " + victoriasJugador1 + " partidas");
+        System.out.println("El jugador 2 ha ganado: " + victoriasJugador2 + " partidas");
+
+        /*
         for (int i = 0; i < partidas; i++) {
             jugador1 = tiradaDados();
             jugador2 = tiradaDados();
@@ -86,6 +153,8 @@ public class Ejercicio2 {
 
         System.out.println("El jugador 1 ha ganado: " + victoriasJugador1);
         System.out.println("El jugador 2 ha ganado: " + victoriasJugador2);
+
+         */
 
 
     }
