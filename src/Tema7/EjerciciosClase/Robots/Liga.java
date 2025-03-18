91ef0a5e3b342eb6911f69a1a545d8c88f8e4cbd
alumnoj <jsolcor457@g.educaand.es>
2025-03-18 17:52:56 +0100

    crx

---
+++ src/Tema7/EjerciciosClase/Robots/Liga.java
@@ -0,0 +1,47 @@
+package Tema7.EjerciciosClase.Robots;
+
+import java.util.ArrayList;
+
+public class Liga<T extends Robot>{
+
+    private String nombre;
+    private ArrayList<T> listaRobots;
+
+    public Liga(String nombre) {
+        this.nombre = nombre;
+        this.listaRobots = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<T> getListaRobots() {
+        return listaRobots;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Liga{");
+        sb.append("nombre='").append(nombre).append('\'');
+        sb.append(", listaRobots=").append(listaRobots);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public void addRobot(T robot){
+        this.listaRobots.add(robot);
+    }
+
+    public void imprimirParticipantes(){
+        for (T robot : this.listaRobots){
+            System.out.println(robot.getNombre() + robot.getEnergia());
+        }
+    }
+
+
+}
---
+++ src/Tema7/EjerciciosClase/Robots/RobotLucha.java
@@ -0,0 +1,30 @@
+package Tema7.EjerciciosClase.Robots;
+
+public class RobotLucha extends Robot {
+
+    private Integer fuerza;
+
+
+    public RobotLucha(String nombre, Integer energia, Integer fuerza) {
+        super(nombre, energia);
+        this.fuerza = fuerza;
+    }
+
+    public Integer getFuerza() {
+        return fuerza;
+    }
+
+    public void setFuerza(Integer fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("RobotLucha{");
+        sb.append("fuerza=").append(fuerza);
+        sb.append(", nombre='").append(nombre).append('\'');
+        sb.append(", energia=").append(energia);
+        sb.append('}');
+        return sb.toString();
+    }
+}
