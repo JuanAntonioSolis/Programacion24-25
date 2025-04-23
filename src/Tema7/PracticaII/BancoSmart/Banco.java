c366016e6a45386e572231406725fd2173781aa2
alumnoj <jsolcor457@g.educaand.es>
2025-04-23 19:52:35 +0200

    llamacuelga

--- src/Tema7/PracticaII/BancoSmart/Banco.java
+++ src/Tema7/PracticaII/BancoSmart/Banco.java
@@ -1,8 +1,9 @@
 package Tema7.PracticaII.BancoSmart;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Banco {
 
@@ -54,14 +55,83 @@ public class Banco {
     public void getTransaccionesImporteMinimo(double valor){
         cuentas.stream()
                 .flatMap(cuenta -> cuenta.getTransacciones().stream())
-                .filter(t -> t.getImporte() >= valor)
+                .filter(t -> t.getImporte() > valor)
                 .sorted(Comparator.comparing(Transaccion::getFecha))
                 .forEach(System.out::println);
     }
 
     public void getIngresosTotales(){
-
+        cuentas.stream()
+                .flatMap(c -> c.getTransacciones().stream())
+                .filter(t -> t.getTipo().equals(TipoTransaccion.INGRESO))
+                .mapToDouble(Transaccion::getImporte)
+                .summaryStatistics()
+                .getSum();
     }
+
+    public void getGastosTotales(){
+        cuentas.stream()
+                .flatMap(c -> c.getTransacciones().stream())
+                .filter(t -> t.getTipo().equals(TipoTransaccion.GASTO))
+                .map(Transaccion::getImporte)
+                .reduce(Double::sum);
+    }
+
+    public void getCuentasPorSaldo(){
+        cuentas.stream()
+                .sorted(Comparator.comparing(Cuenta::getSaldo))
+                .forEach(System.out::println);
+    }
+
+    public void getNumTransaccionesPorCuenta(){
+        Map<String,Long> transPorCuenta = cuentas.stream()
+                .collect(Collectors.groupingBy(Cuenta::getId,Collectors.counting()));
+
+        transPorCuenta.forEach( (k,v) -> System.out.println(k + ": " + v));
+    }
+
+    public void getCuentasActivas(){
+        cuentas.stream()
+                .filter(c -> !c.getTransacciones().isEmpty())
+                .flatMap(c -> c.getTransacciones().stream())
+                .filter(t -> t.getFecha().getMonth().equals(LocalDate.now().getMonth()))
+                .forEach(System.out::println);
+    }
+
+    public void getTransaccionesPorDescripcio(String palabra){
+        Map<String, Set<Transaccion>> transPorDesc = cuentas.stream()
+                .collect(Collectors.toMap(Cuenta::getId, c -> c.getTransacciones().stream()
+                        .filter(t -> t.getDescripcion().contains(palabra))
+                        .collect(Collectors.toSet())));
+        transPorDesc.forEach((k,v) -> {
+            System.out.println(k + " - ");
+            v.stream().forEach( t -> System.out.println(t + ", "));
+            System.out.println();
+        });
+    }
+
+    public void showAnalisisTemporal(){
+        Map<Month, List<Transaccion>> analisis = cuentas.stream()
+                .flatMap(c -> c.getTransacciones().stream())
+                .collect(Collectors.groupingBy(t -> t.getFecha().getMonth()));
+
+        analisis.forEach((k,v) ->{
+            System.out.print(k + " - ");
+            double ingresos = v.stream()
+                    .filter(t -> t.getTipo().equals(TipoTransaccion.INGRESO))
+                    .mapToDouble(Transaccion::getImporte)
+                    .sum();
+            double gastos = v.stream()
+                    .filter(t -> t.getTipo().equals(TipoTransaccion.GASTO))
+                    .mapToDouble(Transaccion::getImporte)
+                    .sum();
+
+            System.out.println("ingresos: " + ingresos + ", gastos: " + gastos + "€");
+        });
+    }
+
+
+
 
 
 }
--- src/Tema7/PracticaII/BancoSmart/Test.java
+++ src/Tema7/PracticaII/BancoSmart/Test.java
@@ -1,8 +1,13 @@
 package Tema7.PracticaII.BancoSmart;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class Test {
 
     public static void main(String[] args) {
+
+        Banco santander = new Banco();
 
         Cliente c1 = new Cliente("11111111A","Juan","juan@gmail.com","111111111",
                 "Huercal","Almeria","España");
@@ -14,6 +19,43 @@ public class Test {
                 "Marley","Bomboclat","Jamaica");
         Cliente c5 = new Cliente("55555555E","Sam","sam@gmail.com","555555555",
                 "Montecastillo","Almeria","Spain");
+
+        Cuenta ct1 = new Cuenta(c1);
+        Cuenta ct2 = new Cuenta(c1);
+        Cuenta ct3 = new Cuenta(c2);
+        Cuenta ct4 = new Cuenta(c2);
+        Cuenta ct5 = new Cuenta(c3);
+        Cuenta ct6 = new Cuenta(c3);
+        Cuenta ct7 = new Cuenta(c4);
+        Cuenta ct8 = new Cuenta(c4);
+        Cuenta ct9 = new Cuenta(c5);
+        Cuenta ct10 = new Cuenta(c5);
+
+        santander.addCuenta(ct1);
+        santander.addCuenta(ct2);
+        santander.addCuenta(ct3);
+        santander.addCuenta(ct4);
+        santander.addCuenta(ct5);
+        santander.addCuenta(ct6);
+        santander.addCuenta(ct7);
+        santander.addCuenta(ct8);
+        santander.addCuenta(ct9);
+        santander.addCuenta(ct10);
+
+        Transaccion t1 = new Transaccion(ct1,60.0, LocalDate.of(2025,3,28),
+                TipoTransaccion.INGRESO,"Corte");
+        Transaccion t2 = new Transaccion(ct3,500.0, LocalDate.of(2025,3,28),
+                TipoTransaccion.GASTO,"PS4");
+        Transaccion t3 = new Transaccion(ct2,1500.0, LocalDate.of(2025,4,1),
+                TipoTransaccion.INGRESO,"Nomina");
+        Transaccion t4 = new Transaccion(ct4,800.0, LocalDate.of(2025,4,1),
+                TipoTransaccion.INGRESO,"NominaAbril");
+
+        santander.getTransaccionesImporteMinimo(800.0);
+
+
+
+
 
 
 
