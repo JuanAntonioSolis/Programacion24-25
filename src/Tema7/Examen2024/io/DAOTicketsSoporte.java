c708980bd086b0038d22cf91e9cb3d6acb354ab9
juana <jsolcor457@g.educaand.es>
2025-05-18 18:48:39 +0200

    DAO finish...

--- src/Tema7/Examen2024/io/DAOTicketsSoporte.java
+++ src/Tema7/Examen2024/io/DAOTicketsSoporte.java
@@ -1,4 +1,21 @@
 package Tema7.Examen2024.io;
 
+import Tema7.Examen2024.models.Tecnico;
+import Tema7.Examen2024.models.Usuario;
+import Tema7.Examen2024.services.ServicioSoporte;
+
+import java.util.List;
+
 public class DAOTicketsSoporte {
+
+    public static void cargarCSV(){
+
+        ServicioSoporte servicioSoporte = new ServicioSoporte();
+
+        List<Usuario> usuarios;
+        List<Tecnico> tecnicos;
+
+
+
+    }
 }
